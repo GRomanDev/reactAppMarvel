0e090bc9102d0b28cc5c0566d4e5b2161f7c6e6e
Roman <grromf@gmail.com>
2022-04-28 20:47:21 +0800

    refac: CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -90,6 +90,7 @@ class CharList extends Component {
                         }}
                         onKeyPress={(e) => {
                             if (e.key === ' ' || e.key === 'Enter') {
+                                e.preventDefault();
                                 this.props.onCharSelected(item.id);
                                 this.focusOnItem(i);
                             }
